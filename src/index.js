c84edf38dcddcc7a860970988a264e7da9368685
Gustavo Nascimento <o_fantasma@hotmail.com.br>
2020-04-08 21:55:10 -0300

    Primeiro tic-tac-toe

--- src/index.js
+++ src/index.js
@@ -1,17 +1,127 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Square({ value, onClick }) {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Restart({ onClick }) {
+  return (
+    <button className="restart" onClick={onClick}>
+      Jogue Novamente
+    </button>
+  );
+}
+
+function Game() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  const [isXNext, setIsXNext] = useState(true);
+  
+  // const nextSymbol = isXNext ? "X" : "O";
+  
+  const winner = calculateWinner(squares);
+
+  return (
+    <div className="container">
+      <div className="game">
+        <div className="game-board">
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+        <div className="game-info">{getStatus()}</div>
+        <div className="restart-button">{renderRestartButton()}</div>
+      </div>
+    </div>
+  );
+
+  function renderSquare(i) {
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => {
+          if (squares[i] != null || winner != null) {
+            return;
+          }
+          const nextSquares = squares.slice();
+          nextSquares[i] = isXNext ? "X" : "O";
+          setSquares(nextSquares);
+
+          setIsXNext(!isXNext);
+        }}
+      />
+    );
+  }
+
+  function renderRestartButton() {
+    return (
+      <Restart
+        onClick={() => {
+          setSquares(Array(9).fill(null));
+          setIsXNext(true);
+        }}
+      />
+    );
+  }
+
+  function getStatus() {
+    if (winner) {
+      return "Vencedor: " + winner;
+    } else if (isBoardFull (squares)) {
+      return "Velha!";
+    } else {
+      return "Próximo Jogador: " + (isXNext ? "X" : "O")
+    }
+  }
+}
+
+function calculateWinner(squares) {
+  const possibleLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < possibleLines.length; i++) {
+    const [a, b, c] = possibleLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function isBoardFull(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] == null) {
+      return false;
+    }
+  }
+  return true;
+}
+
+ReactDOM.render(<Square />, document.getElementById("root"));
+ReactDOM.render(<Game />, document.getElementById("root"));
